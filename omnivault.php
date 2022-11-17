0b2a544503b8e450bd26f818d1dae53d4a5c2fd5
Joak <joejoak@gmail.com>
2022-11-17 13:38:04 -0600

    responsiveness

---
+++ omnivault.php
@@ -0,0 +1,12 @@
+
+        <section class="index-article">
+            <article>
+                <div class="index-h2-container"><h2>¿Qué he creando?</h2></div>
+                <div class="photo-grid">
+                    <img src="img/OMNIVAULT 1.0.png" class="grid1"><div class="grid1-text">OMNIVAULT</div></img>
+                    <img src="" class="grid2">
+                    <img src="" class="grid3">
+                </div>
+                <a href="#" class="more">Portafolio...</a><br>
+            </article>
+        </section>
